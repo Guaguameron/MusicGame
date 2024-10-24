de8076efc4741ac39b827052a600c43fae288e43
Guaguameron <guaguameron@gmail.com>
2024-10-24 23:22:42 +0900

    石台按钮显示

--- Assets/GameMain/System/StartGameSequence.cs
+++ Assets/GameMain/System/StartGameSequence.cs
@@ -1,42 +1,86 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement; 
 
 public class StartGameSequence : MonoBehaviour
 {
-    public Image ReadyImage;
-    public Image GoImage;
-    public float ReadyDisplayTime = 1.5f;  // Ready 显示时间
-    public float GoDisplayTime = 1.0f;     // Go 显示时间
-    public GameObject GameController;      // 游戏控制器
-    public AudioSource GameMusic;          // 游戏音乐音频源
+    public Image ReadyImage; 
+    public Image GoImage; 
+    public float ReadyDisplayTime = 1.5f; 
+    public float GoDisplayTime = 1.0f; 
+    public GameObject GameController;
+    public AudioSource GameMusic; 
+
+    public Button showButton; // 石台按钮
+    public Image imageToShow; // 点击石台后显示的图片
+    public Button jumpSceneButton;
 
     void Start()
-    {
-        // 初始化图像状态为隐藏
+    {           
         ReadyImage.gameObject.SetActive(false);
         GoImage.gameObject.SetActive(false);
         GameController.SetActive(false);
-        GameMusic.Stop();  // 确保游戏音乐未播放
+        GameMusic.Stop();
+
+        showButton.gameObject.SetActive(false); 
+        imageToShow.gameObject.SetActive(false); 
+        jumpSceneButton.gameObject.SetActive(false); 
+
         StartCoroutine(StartSequence());
     }
 
     IEnumerator StartSequence()
     {
-        // 显示 Ready 图像
         ReadyImage.gameObject.SetActive(true);
-        yield return new WaitForSeconds(ReadyDisplayTime);  // 等待 Ready 时间
+        yield return new WaitForSeconds(ReadyDisplayTime); 
         ReadyImage.gameObject.SetActive(false);
 
-        // 显示 Go 图像
+
         GoImage.gameObject.SetActive(true);
-        yield return new WaitForSeconds(GoDisplayTime);  // 等待 Go 时间
+        yield return new WaitForSeconds(GoDisplayTime); 
         GoImage.gameObject.SetActive(false);
-
-        // 开始游戏逻辑
+          
         GameController.SetActive(true);
+               
+        GameMusic.Play();
 
-        // 播放音乐
-        GameMusic.Play();  // 此处开始播放游戏音乐
+        // 检查音乐播放状态并处理暂停的情况
+        yield return StartCoroutine(WaitForMusicToEnd());
+
+        if (!PauseGame.isPaused)
+        {
+            showButton.gameObject.SetActive(true);
+            showButton.onClick.AddListener(OnButtonClick);
+        }
+    }
+
+    IEnumerator WaitForMusicToEnd()
+    {
+        while (GameMusic.isPlaying || PauseGame.isPaused)
+        {
+            yield return null;
+        }
+    }
+
+    void OnButtonClick()
+    {
+        if (!PauseGame.isPaused)
+        {
+            imageToShow.gameObject.SetActive(true); 
+            showButton.gameObject.SetActive(false); 
+
+            jumpSceneButton.gameObject.SetActive(true);
+            jumpSceneButton.onClick.AddListener(JumpToNextScene);
+        }
+    }
+
+
+    void JumpToNextScene()
+    {
+        if (!PauseGame.isPaused)
+        {
+            SceneManager.LoadScene("Development Team");
+        }
     }
 }
